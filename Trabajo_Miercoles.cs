938fbf44cd28754a6c89a937c95c0bb811bfd9c8
RomanVark <jrromero@uamv.edu.ni>
2025-10-15 13:23:42 -0600

    Uso de commit

---
+++ Trabajo_Miercoles.cs
@@ -0,0 +1,12 @@
+using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Escribe tu nombre: ");
+        string nombre = Console.ReadLine();
+
+        Console.WriteLine("¡Hola, " + nombre + "! Bienvenido al programa.");
+    }
+}
